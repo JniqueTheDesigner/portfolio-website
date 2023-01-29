97502931e61b779640cd7914925b8f24b4564935
JniqueTheDesigner <jasminmhamilton@icloud.com>
2023-01-28 23:44:16 -0500

    add jQuery
    add CDN
    add hamburger icon
    add JavaScript menu toggles
    adjust column padding
    add media queries

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log ("Hi I'm Jnique The Designer, Welcome to My Portfolio Site")
+function menuToggle() {
+    var x = document.getElementById( 'myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
